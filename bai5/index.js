3a009baf7ef4cb778adc55ef24c21f0535e2235a
thien trieu <truongthientrieu1996@gmail.com>
2022-01-06 08:46:54 +0700

    bosungbaitapjs

--- bai5/index.js
+++ bai5/index.js
@@ -1,9 +1,16 @@
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i == 0) return false;
+    return num > 1;
+}
 function lietke() {
     var songuyenEl = document.getElementById("songuyen").value * 1
     var ketQua = " "
     var ketQuaEl = document.getElementById("ketqua")
     for (i = 1; i <= songuyenEl; i++) {
-        ketQua += " " + i
+        var a = isPrime(i);
+        if (a)
+            ketQua +=" "+ i; 
     }
     ketQuaEl.innerHTML = ketQua
 }
